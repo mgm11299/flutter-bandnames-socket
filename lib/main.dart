623969d422d8ed47a8ca75d908080cbcf0c22123
Sole <sole.gutierrez@gmail.com>
2021-08-13 22:43:20 +0200

    App Terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:hola/pages/home.dart';
+import 'package:hola/pages/status.dart';
+import 'package:hola/services/socket_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {'home': (_) => HomePage()},
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => SocketService())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {'home': (_) => HomePage(), 'status': (_) => StatusPage()},
+      ),
     );
   }
 }
--- lib/models/band.dart
+++ lib/models/band.dart
@@ -3,8 +3,10 @@ class Band {
   String name;
   int votes;
 
-  Band({required this.id, required this.name, required this.votes});
+  Band({this.id = '', this.name = '', this.votes = 0});
 
-  factory Band.fromMap(Map<String, dynamic> obj) =>
-      Band(id: obj['id'], name: obj['name'], votes: obj['votes']);
+  factory Band.fromMap(Map<String, dynamic> obj) => Band(
+      id: obj.containsKey('id') ? obj['id'] : 'no-id',
+      name: obj.containsKey('name') ? obj['name'] : 'no-name',
+      votes: obj.containsKey('votes') ? obj['votes'] : 'no-votes');
 }
